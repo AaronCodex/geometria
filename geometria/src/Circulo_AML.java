eab7b611ac429b1855d792a31346e4375708200f
Aarón Moreno <aaron.severoochoa@gmail.com>
2020-02-03 11:19:10 +0100

    versión refactorizada

--- geometria/src/Circulo_AML.java
+++ geometria/src/Circulo_AML.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_AML extends FiguraGeometrica_AML {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_AML(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
