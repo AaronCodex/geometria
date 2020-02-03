14c5ea284fd3aff93eaf91946e44e413d7d82e54
Aarón Moreno <aaron.severoochoa@gmail.com>
2020-02-03 11:30:46 +0100

    versión comentada

--- geometria/src/Rectangulo_AML.java
+++ geometria/src/Rectangulo_AML.java
@@ -1,19 +1,35 @@
-
+/**
+ * 
+ * @author Aarón Moreno Lozano
+ * @version 1.2
+ */
 public class Rectangulo_AML extends FiguraGeometrica_AML {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * Es un constructor de la clase rectangulo
+	 * @param tipoFigura le pasas el tipo de figura
+	 * @param lG es un lado del rectángulo
+	 * @param lP el otro lado del rectángulo
+	 */
 	public Rectangulo_AML(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+	
+	/**
+	 * Devuelve el área
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	/**
+	 * Devuelve el perímetro
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
